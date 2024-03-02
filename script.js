b48a6c8b07a656e25320b2ad556c910969c5a4b4
SolankiNilesh <solankinileshop002@outlook.com>
2024-03-02 12:34:04 +0530

    in this cod we make one function and give name to updateColorValueDisplay because its update color to the html page and after that we use for in loop and createlement div circle and append them with colorValueHistory  and use if for in loop if for in loop execute three time so createlemet br elemeant

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     var nextButton = document.getElementById("Next");
     var History = [];
-
+    
     function updateColorValueDisplay(historyObject) {
         var colorValueHistory = document.getElementById("ShowcolorValue");
         colorValueHistory.innerHTML = "";
-
+    
+        var Linebreak = 0;  
         for (var key in historyObject) {
-            var colorValueElement = document.createElement("p");
-            colorValueElement.textContent = key + ": " + historyObject[key];
+            var colorValueElement = document.createElement("div");
+            colorValueElement.className = "circle";
+            colorValueElement.style.backgroundColor = historyObject[key];
             colorValueHistory.appendChild(colorValueElement);
+    
+            Linebreak++;
+            if (Linebreak % 3 === 0) { 
+                colorValueHistory.appendChild(document.createElement("br"));
+            }
         }        
     }
-
     nextButton.addEventListener("click", function () {
         var circles = document.querySelectorAll(".circle");
         var HistoryObject = { bulb_1: ' ', bulb_2: ' ', bulb_3: ' ' };
@@ -26,9 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(History);
         updateColorValueDisplay(HistoryObject);
     });
- 
-});     
-
+});
 function getRandomColor() {
     var letters = "0123456789ABCDEF";
     var color = "#";
